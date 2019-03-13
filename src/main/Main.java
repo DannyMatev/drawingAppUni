09d7d783b4d6ecd2381a836ceae2707447df5db7
penko-outlov <penko.outlov@gmail.com>
2019-03-13 20:24:09 +0200

    Implement drawing of circle with a selected color from the color palette

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,16 @@
 package main;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.StartWithExtensionsType;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -35,6 +40,12 @@ public class Main extends Application {
     private Slider transparencySlider;
 
     private Slider thicknessSlider;
+
+    private Rectangle rectangle;
+
+    private Circle circle;
+
+    private Line line;
 
     public static void main(String[] args) {
         launch(args);
@@ -82,6 +93,56 @@ public class Main extends Application {
 
     private void setupCanvas() {
         mainCanvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        circle = new Circle();
+        rectangle = new Rectangle();
+        line = new Line();
+
+        mainCanvas.setOnMousePressed((event) -> {
+            ToggleButton selectedButton = ((ToggleButton) shapesToggle.getSelectedToggle());
+            String shape = selectedButton != null ? selectedButton.getText() : "";
+
+            switch (shape) {
+                case "Circle":
+                    break;
+                case "Rectangle":
+                    break;
+                case "Line":
+                    line.setStartX(event.getX());
+                    line.setStartY(event.getY());
+                    break;
+                case "Point":
+                    break;
+                case "Polygon":
+                    break;
+            }
+        });
+
+        mainCanvas.setOnMouseReleased((event) -> {
+            ToggleButton selectedButton = ((ToggleButton) shapesToggle.getSelectedToggle());
+            String shape = selectedButton != null ? selectedButton.getText() : "";
+
+            switch (shape) {
+                case "Circle":
+                    break;
+                case "Rectangle":
+                    break;
+                case "Line":
+                    line.setEndX(event.getX());
+                    line.setEndY(event.getY());
+
+                    GraphicsContext graphicsContext = mainCanvas.getGraphicsContext2D();
+                    graphicsContext.setStroke(colorPicker.getValue());
+
+                    graphicsContext.strokeLine(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
+                    break;
+                case "Point":
+                    break;
+                case "Polygon":
+                    break;
+            }
+        });
+
         pane.setRight(mainCanvas);
     }
 
@@ -94,14 +155,33 @@ public class Main extends Application {
 
         ToggleButton circleButton = new ToggleButton("Circle");
         circleButton.setPrefSize(200, 30);
+        circleButton.setOnMouseClicked(event -> {
+            enableControls();
+        });
+
         ToggleButton rectangleButton = new ToggleButton("Rectangle");
         rectangleButton.setPrefSize(200, 30);
+        rectangleButton.setOnMouseClicked(event -> {
+            enableControls();
+        });
+
         ToggleButton lineButton = new ToggleButton("Line");
         lineButton.setPrefSize(200, 30);
+        lineButton.setOnMouseClicked(event -> {
+            enableControls();
+        });
+
         ToggleButton pointButton = new ToggleButton("Point");
         pointButton.setPrefSize(200, 30);
+        pointButton.setOnMouseClicked(event -> {
+            enableControls();
+        });
+
         ToggleButton polygonButton = new ToggleButton("Polygon");
         polygonButton.setPrefSize(200, 30);
+        polygonButton.setOnMouseClicked(event -> {
+            enableControls();
+        });
 
         shapesToggle.getToggles().addAll(circleButton, rectangleButton, lineButton, pointButton, polygonButton);
 
@@ -110,9 +190,22 @@ public class Main extends Application {
         pane.setLeft(controls);
     }
 
+    private void enableControls() {
+        if (shapesToggle.getSelectedToggle() != null) {
+            colorPicker.setDisable(false);
+            transparencySlider.setDisable(false);
+            thicknessSlider.setDisable(false);
+        } else {
+            colorPicker.setDisable(true);
+            transparencySlider.setDisable(true);
+            thicknessSlider.setDisable(true);
+        }
+    }
+
     private void setupColorsMenu() {
         colorPicker = new ColorPicker();
         colorPicker.setPrefSize(200, 30);
+        colorPicker.setDisable(true);
 
         controls.getChildren().add(colorPicker);
     }
@@ -125,6 +218,7 @@ public class Main extends Application {
         transparencySlider.setMajorTickUnit(0.25f);
         transparencySlider.setBlockIncrement(0.1f);
         transparencySlider.setSnapToTicks(true);
+        transparencySlider.setDisable(true);
 
         Label trans = new Label("Transparency");
 
@@ -139,6 +233,7 @@ public class Main extends Application {
         thicknessSlider.setMajorTickUnit(1.0f);
         thicknessSlider.setBlockIncrement(0.1f);
         thicknessSlider.setSnapToTicks(true);
+        thicknessSlider.setDisable(true);
 
         Label trans = new Label("Line thickness");
 
